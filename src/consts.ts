7cc02ed39c5c2508aa39838c17638844c5fe774a
张明昊 <2023010792@secoder.net>
2026-02-24 13:46:49 +0800

    feat: rebuild website with Astro and Tailwind CSS

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = '张明昊 | Minghao Zhang';
+export const SITE_DESCRIPTION = 'Personal website and blog of Minghao Zhang, Computer Science student at Tsinghua University.';
